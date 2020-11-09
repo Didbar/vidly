91f852c68179ba6bd8a842b20315260b0035b0bb
davit <ddidbaridze@gmail.com>
2020-11-09 23:27:31 +0100

    First Exercies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <h1>Hello World!</h1>
-    </main>
+    <div className="App">
+
+    </div>
   );
 }
 
---
+++ src/components/movies.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { deleteMovie, getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+    state = {
+        movies: getMovies()
+    };
+
+    handleDelete = (id) => {
+        const movies = this.state.movies.filter(m => m._id !== id);
+        this.setState({ movies })
+    };
+
+    render() {
+
+        const moviescomponent = this.state.movies.map((movie) => {
+            return (
+                <tr key={movie._id} className="m-2">
+                    <th scope="row">{movie.title}</th>
+                    <th>{movie.genre.name}</th>
+                    <th>{movie.numberInStock}</th>
+                    <th>{movie.dailyRentalRate}</th>
+                    <th className="btn btn-danger btn-sm" onClick={() => this.handleDelete(movie._id)}>Delete</th>
+                </tr>);
+        })
+
+        return (
+            <main className="container">
+                {this.state.movies.length !== 0 ?
+                    <table className="table table-hover">
+                        <caption>Showing {this.state.movies.length} Movies in The Database</caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Genre</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Rate</th>
+                                <th scope="col"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {moviescomponent}
+                        </tbody>
+                    </table>
+                    :
+                    <h2>There Are No Movies In Database</h2>
+                }
+            </main>
+        );
+    }
+
+}
+
+export default Movies;
+
+

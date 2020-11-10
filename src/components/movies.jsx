53746733b33a395748071d83298918941092af1c
davit <ddidbaridze@gmail.com>
2020-11-10 21:27:20 +0100

    Added PAgination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,50 +1,81 @@
 import React, { Component } from 'react';
-import { deleteMovie, getMovies } from '../services/fakeMovieService';
+import { getMovies } from '../services/fakeMovieService';
+import Movie from './movie'
+import Pagination from './common/pagination'
+import { paginate } from '../utils/paginate'
 
 class Movies extends Component {
     state = {
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4,
+        currentPage: 1
     };
+
 
     handleDelete = (id) => {
         const movies = this.state.movies.filter(m => m._id !== id);
         this.setState({ movies })
     };
 
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = { ...movies[index] };
+        movies[index].liked = !movies[index].liked
+        this.setState({
+            movies
+        })
+    }
+    handlePageChange = (page) => {
+        this.setState({
+            currentPage: page
+        })
+    }
+
     render() {
 
-        const moviescomponent = this.state.movies.map((movie) => {
-            return (
-                <tr key={movie._id}>
-                    <th scope="row">{movie.title}</th>
-                    <th>{movie.genre.name}</th>
-                    <th>{movie.numberInStock}</th>
-                    <th>{movie.dailyRentalRate}</th>
-                    <th className="btn btn-danger btn-sm" onClick={() => this.handleDelete(movie._id)}>Delete</th>
-                </tr>);
-        })
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage, movies } = this.state;
+
+        if (count === 0) return <h2>There Are No Movies In Database</h2>
+        const moviesPaginated = paginate(movies, currentPage, pageSize);
 
         return (
             <main className="container">
-                {this.state.movies.length !== 0 ?
-                    <table className="table table-hover">
-                        <caption>Showing {this.state.movies.length} Movies in The Database</caption>
-                        <thead>
-                            <tr>
-                                <th scope="col">Title</th>
-                                <th scope="col">Genre</th>
-                                <th scope="col">Stock</th>
-                                <th scope="col">Rate</th>
-                                <th scope="col"> </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {moviescomponent}
-                        </tbody>
-                    </table>
-                    :
-                    <h2>There Are No Movies In Database</h2>
-                }
+                <table className="table table-hover">
+                    <caption style={{ captionSide: 'top' }}  >Showing {this.state.movies.length} Movies in The Database</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Rate</th>
+                            <th scope="col"> </th>
+                            <th scope="col"> </th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {moviesPaginated.map(movie => {
+                            return <Movie
+                                key={movie._id}
+                                movie={movie}
+                                onDelete={this.handleDelete}
+                                onLike={this.handleLike}
+
+
+                            />
+                        })}
+
+                    </tbody>
+                </table>
+                <Pagination
+                    itemsCount={count}
+                    pageSize={pageSize}
+                    currentPage={currentPage}
+                    onPageChange={this.handlePageChange}
+                />
+
             </main>
         );
     }

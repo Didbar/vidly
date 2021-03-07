43db1e9536e33444762b982f104564006e867c89
Daviti Didbaridze <ddidbaridze@gmail.com>
2021-03-07 23:57:42 +0100

    Add Login and Register Forms

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().email().required().label("Username"),
+    name: Joi.string().required().label("Name"),
+    password: Joi.string().min(5).label("Password"),
+  };
+
+  doSubmit = () => {
+    //Call the server
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form className="col-md-7" onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
